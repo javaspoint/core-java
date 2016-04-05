dfde0b75f38b6e4e0cec9ae5c1a6e5fb8710a387
Sunny Mittal <mittalsunny22@gmail.com>
2016-04-05 11:48:35 +0530

    done Signed-off-by: Sunny Mittal <mittalsunny22@gmail.com>

---
+++ Core-java/src/hello.java
@@ -0,0 +1,17 @@
+/**
+ * Reference     : 
+ * Rule Number(s)   : 
+ * GUI Qualifier : 
+ * Author    : AlpsPSO
+ * Date  : 11:37:38 am
+ */
+
+/**
+ * @author smittal
+ *
+ */
+public class hello {
+public static void main(String[] args) {
+	System.out.println("hello");
+}
+}
