6bf8532883b0f586d8350dc0abeb6f16c6019835
Sunny Mittal <mittalsunny22@gmail.com>
2016-04-05 15:33:19 +0530

    now

---
+++ Core-java/src/test.java
@@ -0,0 +1,17 @@
+/**
+ * Reference     : 
+ * Rule Number(s)   : 
+ * GUI Qualifier : 
+ * Author    : AlpsPSO
+ * Date  : 1:07:57 pm
+ */
+
+/**
+ * @author smittal
+ *
+ */
+public class test {
+public static void main(String[] args) {
+	System.out.println("test");
+}
+}
